caafb89312e82f05cc0c02a1b2503d5d919cdfa3
stevengeorgeharris <mail@stevengeorgeharris.com>
2018-04-17 21:15:06 +0100

    Test gltf

--- src/Three.js
+++ src/Three.js
@@ -2,16 +2,21 @@ import React, { Component } from 'react';
 import * as THREE from 'three';
 import OBJLoader from './loaders/OBJLoader';
 import MTLLoader from './loaders/MTLLoader';
+import ColladaLoader from './loaders/MTLLoader';
+import GLTFLoader from 'three-gltf-loader';
+import { Object3D } from 'three';
 
 // Extend THREE
 OBJLoader(THREE);
 MTLLoader(THREE);
+ColladaLoader(THREE);
 
 const models = [
     {
         obj: process.env.PUBLIC_URL + '/models/sword.obj',
         mtl: process.env.PUBLIC_URL + '/models/sword.mtl',
         json: process.env.PUBLIC_URL + '/models/pump/pump.json',
+        gltf: process.env.PUBLIC_URL + '/models/space/space.gltf'
     }
 ]
 
@@ -31,37 +36,50 @@ export default class Three extends Component {
         const scene = new this.THREE.Scene()
         const camera = new this.THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 100 );
         const renderer = new this.THREE.WebGLRenderer({ antialias: true })
-        const loader = new this.THREE.OBJLoader();
+        const loader = new GLTFLoader();
+        const objLoader = new this.THREE.OBJLoader();
         const mtlLoader = new this.THREE.MTLLoader();
         this.clock = new this.THREE.Clock();
         const grid = new this.THREE.GridHelper( 20, 20, 0xffffff, 0xffffff );
         const pointLight = new this.THREE.PointLight( 0xffffff, 1 );
 
         window.addEventListener('resize', () => this.resizeHandler());
-        
-        mtlLoader.load(
-            models[0].mtl,
-            materials => {
-                materials.preload();
 
-                loader.setMaterials(materials);
-                loader.load(
-                    models[0].obj,
-                    obj => {
-                        
-                        let container = new this.THREE.Object3D();
-                        container.add(obj)
-                        container.scale.set(.005,.005,.005);
-                        scene.add( container );
-        
-                        //this.mixer = new this.THREE.AnimationMixer( obj );
-                        //this.mixer.clipAction( obj.animations[ 0 ] ).play();
-                        
-                        this.start();
-                    },
+        loader.load(
+            models[0].gltf,
+            mesh => {
+                let container = new this.THREE.Object3D();
+                container.position.set(-4,0,8);
+                container.add(mesh.scene);
+
+                scene.add(container);
+
+                this.mixer = new THREE.AnimationMixer(mesh.scene);
+                this.mixer.clipAction(mesh.animations[0]).play();
+
+                mtlLoader.load(
+                    models[0].mtl,
+                    materials => {
+                        materials.preload();
+
+                        objLoader.setMaterials(materials);
+                        objLoader.load(
+                            models[0].obj,
+                            obj => {
+                                
+                                let container = new this.THREE.Object3D();
+                                container.position.set(4,0,-4);
+                                container.add(obj)
+                                container.scale.set(.005,.005,.005);
+                                scene.add( container );                                
+                                
+                                this.start();
+                            },
+                        );
+                    }
                 );
             }
-        );
+        )
 
         camera.position.set( - 5.00, 3.43, 11.31 );
         camera.lookAt( new this.THREE.Vector3( - 1.22, 2.18, 4.58 ) );
@@ -114,7 +132,7 @@ export default class Three extends Component {
     
       renderScene() {
         this.renderer.render(this.scene, this.camera)
-        //this.mixer.update( this.clock.getDelta() );
+        this.mixer.update( this.clock.getDelta() );
       }
     
       render() {
